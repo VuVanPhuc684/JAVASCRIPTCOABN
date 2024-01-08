839ef0ae0655bd5fddb3628813ebbbf4d96a349e
Vũ văn Phúc <phucvuvan63@gmail.com>
2024-01-08 08:01:07 +0700

    them code

---
+++ buoi_hoc/buoi_2/vd1.js
@@ -0,0 +1,6 @@
+var a = prompt('Nhập giá trị của a: ');
+var b = prompt('Nhập giá trị của b: ');
+
+var c =(Math.sqrt(a * a + b * b)).toPrecision(2) ;
+
+document.write("Độ dài cạnh huyền là: " + c);
