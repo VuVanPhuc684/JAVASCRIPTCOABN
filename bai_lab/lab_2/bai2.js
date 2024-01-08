ad086f5fbc1647757a4858065e1934ed70f8517c
Vũ văn Phúc <phucvuvan63@gmail.com>
2024-01-08 08:58:19 +0700

    them

--- bai_lab/lab_2/bai2.js
+++ bai_lab/lab_2/bai2.js
@@ -1,6 +1,6 @@
 var so_m2=3600;
 var so_sao=so_m2/360;
-var so_ha=so_m2/10000;
+var so_ha=so_m2/36000;
 var so_mau=so_m2/10000;
 document.write(" gia tri met vuong ban dau la la:"+so_m2+"m2<br>");
 document.write(" doi sang don vi sao la :"+so_sao+"sao<br>");
