529f357bdf98158f29c97164f317ee2a39ccb8d1
Vũ văn Phúc <phucvuvan63@gmail.com>
2024-01-12 08:24:03 +0700

    them code lab3

---
+++ bai_lab/lab3Ph34858/bai2.js
@@ -0,0 +1,7 @@
+for (var i = 1; i <= 9; i++) {
+    document.write('bang cuu chuong : '+i+'<br>');
+    for (var j = 1; j <= 9; j++) {
+      document.write(i + " x " + j + " = " + i * j+"<br>");
+    }
+    document.write("");
+  }
