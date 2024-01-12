55d8f9583d905b9d5f1aae0bec50ca5e2cfeff45
Vũ văn Phúc <phucvuvan63@gmail.com>
2024-01-12 08:34:35 +0700

    them code

--- bai_lab/lab3Ph34858/bai2.js
+++ bai_lab/lab3Ph34858/bai2.js
@@ -1,7 +1,21 @@
+// cách 1 
 for (var i = 1; i <= 9; i++) {
     document.write('bang cuu chuong : '+i+'<br>');
     for (var j = 1; j <= 9; j++) {
       document.write(i + " x " + j + " = " + i * j+"<br>");
     }
     document.write("");
-  }
+}
+
+// cách 2 :
+var i = 1;
+while (i <= 9) {
+    document.write('bang cuu chuong : ' + i + '<br>');
+    var j = 1;
+    while (j <= 9) {
+        document.write(i + ' x ' + j + ' = ' + i * j + '<br>');
+        j++;
+    }
+    document.write('');
+    i++;
+}
