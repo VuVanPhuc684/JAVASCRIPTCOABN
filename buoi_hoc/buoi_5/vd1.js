5d2ff835bd059c80203f85b7a1b7368dacbdd183
Vũ văn Phúc <phucvuvan63@gmail.com>
2024-01-22 08:58:01 +0700

    theem code

---
+++ buoi_hoc/buoi_5/vd1.js
@@ -0,0 +1,5 @@
+// truy xuất đến id 
+document.getElementById("a").innerHTML = "hello, i am phuc's";
+// truy xuất đến thẻ dùng tagName
+document.getElementsByTagName("p")[0].innerHTML = "tôi là vanphuc";
+
